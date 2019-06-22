64edc2c0cc01182ef7f85c522baa325206bd4f12
ayungavis <ayungavis@gmail.com>
2019-06-22 18:54:09 +0700

    Initial commit

---
+++ src/main/java/com/ayungavis/cashierspring/rest/MenuController.java
@@ -0,0 +1,14 @@
+package com.ayungavis.cashierspring.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/menu")
+public class MenuController {
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "Hello, world!";
+	}
+}
